117cb246cba17a5e9cd491f9421ed0ff42879cf1
nurastuti <nurastuti899@gmail.com>
2018-08-26 13:30:34 +0700

    v-01

---
+++ app/src/main/java/com/nur/project_uas/Server.java
@@ -0,0 +1,8 @@
+package com.nur.project_uas;
+/**
+ * Created by Nur Astuti on 13/08/2018.
+ */
+
+public class Server {
+    public static final String URL = "http://sasmitoh.nitarahmawati.my.id/";
+}
